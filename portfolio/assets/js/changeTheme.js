411ef6622141c8b2524a9aa937cc062fd44c7aa3
Alexey Gorbenko <a.v.gor@mail.ru>
2022-01-30 09:01:08 +0300

    refactor: move changeTheme function to separate file

--- portfolio/assets/js/changeTheme.js
+++ portfolio/assets/js/changeTheme.js
@@ -1,2 +1,17 @@
 'use strict';
 
+export default function changeTheme(event) {
+  const themes = ['dark-theme', 'light-theme'];
+  const classesToChange = ['.body', '.header-container', '.hero-container', '.hire-me', '.section-title', '.sec-title-span',
+'.portfolio-buttons', '.video-play-button', '.price-card', '.contacts-container', '.footer-container'];
+
+  // Change icon in header
+  event.target.classList = (event.target.classList == themes[0]+'-icon') ? themes[1]+'-icon' : themes[0]+'-icon';
+
+  // Change styles on page
+  if (event.target.classList == themes[1]+'-icon') {
+    classesToChange.forEach((element) => document.querySelectorAll(`${element}`).forEach((item) => item.classList.add(`${themes[1]}`)))
+  } else {
+    classesToChange.forEach((element) => document.querySelectorAll(`${element}`).forEach((item) => item.classList.remove(`${themes[1]}`)))
+  }
+}
--- portfolio/assets/js/index.js
+++ portfolio/assets/js/index.js
@@ -5,6 +5,7 @@ import changeClassActive from './changeClassActive.js';
 import changeImage from './changeImage.js';
 import preloadSeasonsImages from './preloadSeasonsImages.js';
 import getTranslate from './getTranslate.js';
+import changeTheme from './changeTheme.js';
 
 // Hamburger menu
 document.querySelector('.nav-list').addEventListener('click', toggleMenu);
@@ -24,21 +25,4 @@ languageGroup.addEventListener('click', getTranslate);
 
 // Change theme
 const changeThemeGroup = document.querySelector('.change-theme');
-
-function changeTheme(event) {
-  const themes = ['dark-theme', 'light-theme'];
-  const classesToChange = ['.body', '.header-container', '.hero-container', '.hire-me', '.section-title', '.sec-title-span',
-'.portfolio-buttons', '.video-play-button', '.price-card', '.contacts-container', '.footer-container'];
-
-  // Change icon in header
-  event.target.classList = (event.target.classList == themes[0]+'-icon') ? themes[1]+'-icon' : themes[0]+'-icon';
-
-  // Change styles on page
-  if (event.target.classList == themes[1]+'-icon') {
-    classesToChange.forEach((element) => document.querySelectorAll(`${element}`).forEach((item) => item.classList.add(`${themes[1]}`)))
-  } else {
-    classesToChange.forEach((element) => document.querySelectorAll(`${element}`).forEach((item) => item.classList.remove(`${themes[1]}`)))
-  }
-}
-
 changeThemeGroup.addEventListener('click', changeTheme);
