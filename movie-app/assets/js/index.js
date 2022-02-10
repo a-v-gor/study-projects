7c49ed2f18da9a7c598bd9329fa52acaff712099
Alexey Gorbenko <a.v.gor@mail.ru>
2022-02-10 21:19:21 +0300

    feat: add overview in movie card

--- movie-app/assets/js/index.js
+++ movie-app/assets/js/index.js
@@ -1,4 +1,4 @@
-// https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=f5a4c4a8d947d8097d8aba9c57c93c96
+'use strict';
 
 const url = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=f5a4c4a8d947d8097d8aba9c57c93c96';
 const moviesCardsContainer = document.querySelector('.movies-cards');
@@ -19,6 +19,9 @@ function showData(data) {
       <img class="movie-poster" src=${imgPath+pathPosterImg} alt="${element['title']} poster">
       <div class="container-movie-title">
         <h3 class="movie-title">${element['title']}</h3>
+      </div>
+      <div class="container-movie-overview">
+        <p class="movie-overview">${element['overview']}</p>
       </div>
     </article>
     `;
