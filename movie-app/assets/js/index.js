9f8628fd87030c562cb8b365fbd5ded8fa7725c8
Alexey Gorbenko <a.v.gor@mail.ru>
2022-02-10 16:18:07 +0300

    :feat: add movies cards section

---
+++ movie-app/assets/js/index.js
@@ -0,0 +1,28 @@
+// https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=f5a4c4a8d947d8097d8aba9c57c93c96
+
+const url = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=f5a4c4a8d947d8097d8aba9c57c93c96';
+const moviesCardsContainer = document.querySelector('.movies-cards');
+const imgPath = 'https://image.tmdb.org/t/p/w300'
+
+async function getData() {
+  const res = await fetch(url);
+  const data = await res.json();
+  showData(data);
+}
+
+function showData(data) {
+  data['results'].forEach(element => {
+    console.log(element);
+    const pathPosterImg = element['poster_path'];
+    const movieCard = `
+    <article class="movie-card">
+      <img class="movie-poster" src=${imgPath+pathPosterImg} alt="${element['title']} poster">
+      <div class="container-movie-title">
+        <h3 class="movie-title">${element['title']}</h3>
+      </div>
+    </article>
+    `;
+    moviesCardsContainer.insertAdjacentHTML('beforeend', movieCard);
+  });
+}
+getData();
