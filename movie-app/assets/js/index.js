2c03cf8170479097256e61bfdbc48b432ea76e2e
Alexey Gorbenko <a.v.gor@mail.ru>
2022-02-11 23:10:06 +0300

    :feat: add movie search

--- movie-app/assets/js/index.js
+++ movie-app/assets/js/index.js
@@ -1,18 +1,19 @@
 'use strict';
 
-const url = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=f5a4c4a8d947d8097d8aba9c57c93c96';
+const startUrl = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=f5a4c4a8d947d8097d8aba9c57c93c96';
 const moviesCardsContainer = document.querySelector('.movies-cards');
-const imgPath = 'https://image.tmdb.org/t/p/w300'
+const imgPath = 'https://image.tmdb.org/t/p/w300';
+const searchForm = document.querySelector('.search-form');
+const search = document.querySelector('.search');
+const moviesСards = document.querySelector('.movies-cards');
 
-async function getData() {
-  const res = await fetch(url);
-  const data = await res.json();
-  showData(data);
+getData(startUrl);
+function getData(url) {
+  fetch(url).then(res => res.json()).then(data => showData(data));
 }
 
 function showData(data) {
   data['results'].forEach(element => {
-    console.log(element);
     const pathPosterImg = element['poster_path'];
     const movieCard = `
     <article class="movie-card">
@@ -28,4 +29,17 @@ function showData(data) {
     moviesCardsContainer.insertAdjacentHTML('beforeend', movieCard);
   });
 }
-getData();
+
+function findMovie () {
+  moviesСards.innerHTML = '';
+  if (search.value) {
+    getData(`https://api.themoviedb.org/3/search/movie?query=${search.value}&api_key=f5a4c4a8d947d8097d8aba9c57c93c96`);
+  } else {
+    getData(startUrl);
+  }
+}
+
+searchForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  findMovie();
+});
