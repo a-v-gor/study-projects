05b3dea896aefd49e88618f82185929d291a1176
Alexey Gorbenko <a.v.gor@mail.ru>
2022-07-22 20:47:04 +0300

    feat: add API key

--- src/components/controller/appLoader.js
+++ src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: 'da467788639f495ea4aa92b93582ee8f', // получите свой ключ https://newsapi.org/
         });
     }
 }
