18283b798bf2aa29205db7baf4f3b4888d8f84f3
Alexey Gorbenko <a.v.gor@mail.ru>
2022-07-26 19:43:37 +0300

    feat: rename appLoader from .js to .ts

--- src/components/controller/appLoader.ts
+++ src/components/controller/appLoader.ts
@@ -7,5 +7,12 @@ class AppLoader extends Loader {
         });
     }
 }
+// class AppLoader extends Loader {
+//     constructor() {
+//         super('https://newsapi.org/v2/', {
+//             apiKey: 'da467788639f495ea4aa92b93582ee8f', // получите свой ключ https://newsapi.org/
+//         });
+//     }
+// }
 
 export default AppLoader;
