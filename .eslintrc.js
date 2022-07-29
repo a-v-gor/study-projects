f7a7b8b7d0ee687aab772b9716f6d1d59133bc9f
Alexey Gorbenko <a.v.gor@mail.ru>
2022-07-29 14:49:47 +0300

    refactor: update npm, edit eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,8 +2,8 @@ module.exports = {
   extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
-  rules: {
-    "@typescript-eslint/rule-name": "error"
-  },
+  // rules: {
+  //   "@typescript-eslint/rule-name": "error"
+  // },
   root: true,
 };
