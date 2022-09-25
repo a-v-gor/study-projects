e1fd041acc7a47b851ff847dd0fe344c9874d6ca
Alexey Gorbenko <a.v.gor@mail.ru>
2022-09-25 17:23:32 +0300

    feat: make layout for header on main page

---
+++ online-zoo/pages/main/script.js
@@ -0,0 +1,17 @@
+'use strict';
+
+function changeClassActive (event) {
+  
+  if (!event.target.classList.toString().endsWith('_active') && event.target.innerHTML) {
+    const nonActiveClass = event.target.classList.toString();    
+    const activeClass = nonActiveClass+'_active';
+    const activeElement = document.querySelector(`.${activeClass}`);
+    activeElement.classList.remove(activeClass);
+    activeElement.classList.add(nonActiveClass);
+    event.target.classList.remove(nonActiveClass);
+    event.target.classList.add(activeClass);
+  }
+}
+
+const navigation = document.querySelector('.nav-ul');
+navigation.addEventListener('click', changeClassActive);
