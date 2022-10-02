4cfdfc03252b25de6348413d405b49aa78d3c3f7
Alexey Gorbenko <a.v.gor@mail.ru>
2022-10-02 21:41:20 +0300

    refactor: continue make pick-and-feed section on donate page

--- online-zoo/pages/donate/script.js
+++ online-zoo/pages/donate/script.js
@@ -1,3 +1,14 @@
 'use strict';
 
+function checkLength() {
+
+  this.value = this.value.length > 4 ? this.value.slice(-4) : this.value
+}
+
+const amountNumber = document.querySelector('.pick-and-feed__amount-number');
+
+
+
+amountNumber.addEventListener('input', checkLength);
+
 // alert('Страница `Donate` в работе. Если не сложно, отложите, пожалуйста, проверку. Спасибо.')
