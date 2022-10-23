c67e2df79d4e8f866098c97f5a746f65ee0b543c
Alexey Gorbenko <a.v.gor@mail.ru>
2022-10-23 21:10:19 +0300

    init: start rss gem puzzle task

---
+++ rss-gem-puzzle/src/assets/js/createBody.js
@@ -0,0 +1,5 @@
+'use strict';
+
+export function createBody() {
+  document.body.innerHTML = '<h1>RSS Gem Puzzle</h1>'
+}
---
+++ rss-gem-puzzle/src/index.js
@@ -0,0 +1,5 @@
+'use strict';
+
+import { createBody } from "./assets/js/createBody";
+
+createBody();
