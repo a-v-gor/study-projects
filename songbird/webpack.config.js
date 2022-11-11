f3d4006ee42bc8949ab6cab73cce176f59dfac25
Alexey Gorbenko <a.v.gor@mail.ru>
2022-11-11 21:01:48 +0300

    refactor: add eslint, stylelint and prettier

--- songbird/webpack.config.js
+++ songbird/webpack.config.js
@@ -1,36 +1,40 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const path = require('path');
+const StylelintPlugin = require("stylelint-webpack-plugin");
+const path = require("path");
 
 module.exports = {
-  mode: 'development',
-  entry: path.resolve(__dirname, 'src/index.js'),
+  mode: "development",
+  entry: path.resolve(__dirname, "src/index.js"),
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'index.js',
+    path: path.resolve(__dirname, "dist"),
+    filename: "index.js",
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, 'src/index.html')
+      template: path.resolve(__dirname, "src/index.html"),
     }),
-    new MiniCssExtractPlugin()
+    new MiniCssExtractPlugin(),
+    new StylelintPlugin({
+      files: [path.resolve(__dirname, "src/**/*.{scss, css}")],
+    })
   ],
   module: {
     rules: [
       {
-        test: /\.s[ac]ss$/i,
+        test: /\.(s[ac]ss|css)$/i,
         use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      }
+        type: "asset/resource",
+      },
     ],
   },
   devServer: {
     port: 3000,
     static: {
-      directory: path.join(__dirname, 'src'),
+      directory: path.join(__dirname, "src"),
     },
   },
 };
