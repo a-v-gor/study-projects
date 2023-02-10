6f7efd95db2b3f6d08f6cbac561861e89008a60e
Alexey Gorbenko <a.v.gor@mail.ru>
2023-02-10 15:14:55 +0300

    refactor: start make game logic

--- songbird/src/assets/js/returnGamePage.js
+++ songbird/src/assets/js/returnGamePage.js
@@ -1,8 +1,15 @@
-import  { createPageHTML } from "./createPageHTML";
-import  { returnGameBody } from "./returnGameBody";
-import  { makeHeaderActive } from "./makeHeaderActive";
+import { createPageHTML } from "./createPageHTML";
+import { returnGameBody } from "./returnGameBody";
+import { makeHeaderActive } from "./makeHeaderActive";
+import birdsData from "./birds";
 
 export function returnGamePage() {
   createPageHTML(returnGameBody());
   makeHeaderActive();
+  const variants = {};
+  for (let i = 0; i < birdsData.length; i++) {
+    variants[`${i}`] = birdsData[i];
+  }
+
+  console.log(variants);
 }
