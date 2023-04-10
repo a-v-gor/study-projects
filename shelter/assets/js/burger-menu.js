03f23d6e5403d16ca258c886d6e050479ea23b7b
Alexey Gorbenko <a.v.gor@mail.ru>
2023-04-10 21:21:09 +0300

    fix: fix style problem

--- shelter/assets/js/burger-menu.js
+++ shelter/assets/js/burger-menu.js
@@ -1,51 +1,49 @@
-const substrateLayer = document.querySelector(".substrate-burger-menu");
-const inputHamburger = document.querySelector(".nav__menu-toggle");
-const navItems = document.querySelector(".nav__list");
-
 export function burgerMenu() {
-  function makeActive(elem) {
+  const substrateLayer = document.querySelector(".substrate-burger-menu");
+  const inputHamburger = document.querySelector(".nav__menu-toggle");
+  const navItems = document.querySelector(".nav__list");
+
+  function makeActiveBurg(elem) {
     const strClass = elem.classList;
     const strActiveClass = strClass + "_active";
     elem.classList.remove(strClass);
     elem.classList.add(strActiveClass);
   }
 
-  function makeUnactive(elem) {
+  function makeUnactiveBurg(elem) {
     const strActiveClass = elem.classList;
     const strUnactiveClass = strActiveClass.value.slice(0, -7);
-    elem.classList.remove(strActiveClass);
-    elem.classList.add(strUnactiveClass);
+    elem.classList.replace(strActiveClass, strUnactiveClass);
   }
 
-  function disableScroll() {
+  function disableScrollBurg() {
     const bodyClass = document.body.classList;
     const bodyClassStopScroll = document.body.classList + "_stop-scroll";
-    document.body.classList.remove(bodyClass);
-    document.body.classList.add(bodyClassStopScroll);
+    document.body.classList.replace(bodyClass, bodyClassStopScroll);
   }
   
-  function enableScroll() {
+  function enableScrollBurg() {
     const bodyClassStopScroll = document.body.classList;
     const strClassEnable = document.body.classList.value.slice(0, -12);
-    document.body.classList.remove(bodyClassStopScroll);
-    document.body.classList.add(strClassEnable);
+    document.body.classList.replace(bodyClassStopScroll, strClassEnable);
   }
   
-  function blockPage() {
-    makeActive(substrateLayer);
+  function blockPageBurg() {
+    makeActiveBurg(substrateLayer);
   }
   
-  function unBlockPage() {
-    makeUnactive(substrateLayer);
+  function unBlockPageBurg() {
+    makeUnactiveBurg(substrateLayer);
   }
 
   function checkBurger() {
+    console.log('checkburger');
     if (inputHamburger.checked) {
-      disableScroll();
-      blockPage();
+      disableScrollBurg();
+      blockPageBurg();
     } else {
-      enableScroll();
-      unBlockPage();
+      enableScrollBurg();
+      unBlockPageBurg();
     }
   }
 
@@ -56,14 +54,18 @@ export function burgerMenu() {
     };  
   };
 
-  function hideBurgerMenu (event) {
+  function hideBurgerMenu(event) {
     const navigationItemsArr = Array.prototype.slice.call(document.querySelectorAll(".nav__link"));
     if ((inputHamburger.checked) && ((navigationItemsArr.includes(event.target)) || event.target == document.querySelector(".nav__link_active"))) {
       toggleMenu();
     }
   }
 
+  function uncheckMenu() {
+    inputHamburger.checked = false;
+  };
+
   inputHamburger.addEventListener("input", checkBurger);
-  substrateLayer.addEventListener("click", toggleMenu);
+  substrateLayer.addEventListener("click", uncheckMenu);
   navItems.addEventListener("click", hideBurgerMenu);
 }
