b22ef03066b30c2927bd360cb46d67c42c0a9d53
Alexey Gorbenko <a.v.gor@mail.ru>
2023-04-10 11:56:34 +0300

    feat: add autogeneration shuffled cards on pets page

--- shelter/assets/js/pagination.js
+++ shelter/assets/js/pagination.js
@@ -1,18 +1,19 @@
 import { getWinWidth } from "./get-win-width.js";
+import { generatePetsCard } from "./generate-pets-card.js";
 
 export function pagination() {
   let winWidth;
-  let cardsIDs = [];
   let petsData;
-  let pages = 0;
+  let numOfPages = 0;
 
   function createCardIDs() {
+    const cardsIDs = [];
     for (let i = 0; i < 7; i++) {
       for (let j = 0; j < 8; j++) {
         cardsIDs.push(j);
       }
     }
-    console.log(cardsIDs);
+    return cardsIDs;
   }
 
   function startPagination() {
@@ -26,9 +27,9 @@ export function pagination() {
         
         response.json().then(function(data) {
           petsData = data;
-          createCardIDs();
           changeWinWidth();
-          getNumOfPages();
+          setNumOfPages();
+          generateCardsPages(returnFormedPages());
         })
       }
     )
@@ -44,12 +45,43 @@ export function pagination() {
     }
   }
 
-  function getNumOfPages() {
-    pages = (winWidth = "desktop") ? 6 : (winWidth = "tablet") ? 8 : 16;
-    console.log('pages = ' + pages);
+  function setNumOfPages() {
+    numOfPages = (winWidth = "desktop") ? 6 : (winWidth = "tablet") ? 8 : 16;
+    console.log('numOfPages = ' + numOfPages);
   }
 
-  
+  function shuffleArray(arr) {
+    const sourceArr = [].concat(arr);
+    const result = [];
+    for (let i = 0; i < arr.length; i++) {
+      result.push(sourceArr.splice([Math.floor((Math.random() * sourceArr.length))], 1));
+    }
+    return result.flat(1);
+  }
+
+  function returnFormedPages() {
+    const pages = [];
+    const cardIDs = createCardIDs();
+    for (let i = 0; i < numOfPages; i++) {
+      const page = cardIDs.splice(0, 48 / numOfPages);
+      pages.push(shuffleArray(page));
+    };
+    console.log(pages);
+    return pages;
+  }
+
+  function generateCardsPages(arrPagesIDs) {
+    for (let i = 0; i < arrPagesIDs.length; i++){
+      const cardsPage = document.createElement("div");
+      cardsPage.classList = "our-friends__cards-page";
+      cardsPage.id = `page-${i}`;
+      for (let j = 0; j < 48 / numOfPages; j++) {
+        const card = generatePetsCard(arrPagesIDs[i][j], petsData);
+        cardsPage.insertAdjacentHTML("beforeend", card);
+      }
+      document.querySelector(".our-friends__cards-pages-wrap").appendChild(cardsPage);
+    }
+  }
 
   document.addEventListener("DOMContentLoaded", startPagination);
   window.addEventListener("resize", changeWinWidth);
