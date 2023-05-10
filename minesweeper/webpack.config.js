f24ed5812e2d7fc5c569ffe3a1f994eca1f5c484
Alexey Gorbenko <a.v.gor@mail.ru>
2023-05-10 21:54:17 +0300

    init: start minesweeper task

---
+++ minesweeper/webpack.config.js
@@ -0,0 +1,40 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const StylelintPlugin = require("stylelint-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: path.resolve(__dirname, "src/index.js"),
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "index.js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "src/index.html"),
+    }),
+    new MiniCssExtractPlugin(),
+    new StylelintPlugin({
+      files: [path.resolve(__dirname, "src/**/*.{scss, css}")],
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(s[ac]ss|css)$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif|ico)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+  devServer: {
+    port: 3000,
+    static: {
+      directory: path.join(__dirname, "src"),
+    },
+  },
+};
