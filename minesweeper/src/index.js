f20867e6c3d21a87a0a800587b08200d519d3e1a
Alexey Gorbenko <a.v.gor@mail.ru>
2023-05-11 20:52:02 +0300

    refactor: start layout

---
+++ minesweeper/src/index.js
@@ -0,0 +1,6 @@
+import "./assets/css/normalize.css";
+import "./assets/css/main.scss";
+import  { returnStartPage } from "./assets/js/returnStartPage";
+
+
+document.addEventListener("DOMContentLoaded", returnStartPage());
