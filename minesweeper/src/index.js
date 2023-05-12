92c7d74d20a11957e215292ea5e4237849225488
Alexey Gorbenko <a.v.gor@mail.ru>
2023-05-12 15:24:20 +0300

    feat: add layout for desktop

--- minesweeper/src/index.js
+++ minesweeper/src/index.js
@@ -1,5 +1,5 @@
 import './assets/css/normalize.css';
 import './assets/css/main.scss';
-import returnStartPage from './assets/js/returnStartPage.js';
+// import returnStartPage from './assets/js/returnStartPage.js';
 
-document.addEventListener('DOMContentLoaded', returnStartPage());
+// document.addEventListener('DOMContentLoaded', returnStartPage());
