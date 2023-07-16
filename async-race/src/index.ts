cd35011f8d6d4dbceb6b0180b4d44bd321249c65
Alexey Gorbenko <a.v.gor@mail.ru>
2023-07-16 20:47:05 +0300

    feat: num of cars and num of winners add on page from server

--- async-race/src/index.ts
+++ async-race/src/index.ts
@@ -1,3 +1,24 @@
-const a = 'async race';
+const baseUrl = 'http://127.0.0.1:3000';
 
-console.log(a);
+async function setNumOfCars() {
+  fetch(`${baseUrl}/garage?_limit=10`)
+    .then((response) => response.headers.get('X-Total-Count'))
+    .then((result) => {
+      const numOfCarsText: HTMLSpanElement = <HTMLSpanElement> document.getElementById('cars-num-text');
+      numOfCarsText.innerText = String(result);
+    })
+    .catch((error) => error.message);
+}
+
+async function setNumOfWinners() {
+  fetch(`${baseUrl}/winners?_limit=7`)
+    .then((response) => response.headers.get('X-Total-Count'))
+    .then((result) => {
+      const numOfWinnersText: HTMLSpanElement = <HTMLSpanElement> document.getElementById('winners-num-text');
+      numOfWinnersText.innerText = String(result);
+    })
+    .catch((error) => error.message);
+}
+
+setNumOfCars();
+setNumOfWinners();
