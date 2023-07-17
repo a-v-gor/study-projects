7e91c9dc8d10b8a7502a39901e185525107cf185
Alexey Gorbenko <a.v.gor@mail.ru>
2023-07-17 16:31:14 +0300

    feat: add switch view by pressing buttons

--- async-race/src/index.ts
+++ async-race/src/index.ts
@@ -1,4 +1,7 @@
+import './assets/scss/style.scss';
+
 const baseUrl = 'http://127.0.0.1:3000';
+const btnsArea: HTMLDivElement = <HTMLDivElement> document.querySelector('.view-btns');
 
 async function setNumOfCars() {
   fetch(`${baseUrl}/garage?_limit=10`)
@@ -20,5 +23,32 @@ async function setNumOfWinners() {
     .catch((error) => error.message);
 }
 
-setNumOfCars();
-setNumOfWinners();
+function startGame() {
+  setNumOfCars();
+  setNumOfWinners();
+}
+
+function changeView(evt: Event) {
+  const viewBtns: NodeListOf<HTMLButtonElement> = btnsArea.querySelectorAll('button');
+  const btnGarage: HTMLButtonElement = viewBtns[0];
+  const btnWinners: HTMLButtonElement = viewBtns[1];
+  const viewSections: NodeListOf<HTMLElement> = document.querySelectorAll('.view');
+  const viewGarage: HTMLElement = viewSections[0];
+  const viewWinners: HTMLElement = viewSections[1];
+
+  function showHide(sectToHide: HTMLElement, sectToShow: HTMLElement) {
+    if (!sectToHide.classList.contains('view_unactive')) {
+      sectToHide.classList.add('view_unactive');
+      sectToShow.classList.remove('view_unactive');
+    }
+  }
+
+  if (evt.target === btnGarage) {
+    showHide(viewWinners, viewGarage);
+  } else if (evt.target === btnWinners) {
+    showHide(viewGarage, viewWinners);
+  }
+}
+
+document.addEventListener('DOMContentLoaded', startGame);
+btnsArea.addEventListener('click', changeView);
